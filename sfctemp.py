f41efadc9b2308b0ff3ca4ec6d7662bc97da0b1b
andieay <48040105+andieay@users.noreply.github.com>
2021-01-13 23:32:29 +0800

    Create sfctemp.py

---
+++ sfctemp.py
@@ -0,0 +1,35 @@
+import xarray as xr
+  
+# open file
+data = xr.open_dataset("sfctemp.nc")
+print(data["tempsfc"])
+print(data.dims)
+print(data.coords)
+print(data.coords["lat"])
+print(data.variables)
+
+# attributes
+print(data["tempsfc"].attrs)
+print(data["tempsfc"].attrs["units"])
+minT = data["tempsfc"].attrs["valid_range"][0]
+maxT = data["tempsfc"].attrs["valid_range"][1]
+print(minT, maxT)
+
+# subscript
+print(data["tempsfc"][2:6, 3, 4:9:2]) # index
+print(data["tempsfc"][{"time":range(2,6),"lat":3,"lon":range(4,9,2)}]) # index
+print(data["tempsfc"].loc[{"time":"2017-01-01T12:00:00", "lat": 80.0, "lon": np.arange(15.0, 25.0, 2.5)}])
+print(data["tempsfc"][{"time":2, "lat":4, "lon":6}])
+
+# modify values
+data["tempsfc"][{"time":2, "lat":4, "lon":6}] = 250
+data["tempsfc"].loc[{"time":"2017-01-01T12:00:00", "lat": 80.0, "lon": 15.0}] = 250
+print(data["tempsfc"][{"time":2, "lat":4, "lon":6}])
+
+# extract values (certain lat/lon)
+lon_c = data["lon"]
+lat_c = data["lat"]
+required_lon = lon_c[np.logical_and(40.0 <= lon_c, lon_c <= 50.0)] # [40.0 42.5 45.0 47.5 50.0]
+required_lat = lat_c[lat_c <= 10.0] # [10.0 7.5 5.0 2.5 0.0 ... -82.5 -85.0 -87.5 -90.0]
+print(data["tempsfc"].loc[{"time":"2017-01-01T12:00:00", "lat":required_lat, "lon":required_lon}])
+
